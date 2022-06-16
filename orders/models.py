98d1f3f49fed213d55bb542f0a1f1b446a389821
Gennady Podrezov <gennady@gennadys-air.myfiosgateway.com>
2022-06-15 20:21:31 -0400

    Before adding a payment bithces

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from tkinter import CASCADE
 from django.db import models
+from accounts.models import UserAddress
 from cart.models import Cart
 
 # Create your models here.
@@ -21,8 +22,11 @@ class Order(models.Model):
     final_price = models.DecimalField(default=0.00, max_digits=1000, decimal_places=2)
     order_id = models.CharField(max_length=120, default='ABC', unique=True)
     status = models.CharField(max_length=120, choices=STATUS_CHOICES, default='started')
+    user_shipping = models.ForeignKey(UserAddress, null=True, on_delete=models.CASCADE)
+    user_biling = models.CharField(max_length=350, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    
 
     def __str__(self):
         return self.order_id
